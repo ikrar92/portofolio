23c6c3a023f39d44b2d5dc0d703a34230ae2dc16
ikrar92 <ikrarjundanafza92@gmail.com>
2026-08-04 07:50:01 +0700

    Fix typo in vite.config.js import path

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs.plugin-vue'
+import vue from '@vitejs/plugin-vue' // <-- Pastikan pakai slash / bukan titik .
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
@@ -7,5 +7,5 @@ export default defineConfig({
     vue(),
     tailwindcss(),
   ],
-  base: '/', // <-- Pastikan nilainya adalah '/'
+  base: '/',
 })
